050450adc1c8445906ddbb8809f3a6a1ba9c7ed6
Utkweb <us196000@gmail.com>
2022-08-23 01:24:02 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/main";
-import Admin from './components/admin';
+import Admin from "./components/admin";
 import User from "./components/user";
 import Login from "./components/main/Login";
 import SignUp from "./components/main/SignUp";
@@ -11,32 +11,36 @@ import ManageUser from "./components/admin/ManageUser";
 import AdminProfile from "./components/admin/AdminProfile";
 import Userprofile from "./components/user/Userprofile";
 import Blog from "./components/blog";
+import { useState } from "react";
+import { UserProvider } from "./components/main/UseContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user"))
+  );
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Main />} path="main">
-          <Route element={<Login />} path="login" />
-          <Route element={<SignUp />} path="signup" />
-        </Route>
+    <UserProvider user={currentUser}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Main />} path="main">
+            <Route element={<Login />} path="login" />
+            <Route element={<SignUp />} path="signup" />
+          </Route>
 
-        <Route element={<Admin />} path="admin">
-          <Route element={<Dashboard />} path="dashboard" />
-          <Route element={<ManageUser />} path="manageuser" />
-          <Route element={<AdminProfile />} path="adminprofile" />
-        </Route>
+          <Route element={<Admin />} path="admin">
+            <Route element={<Dashboard />} path="dashboard" />
+            <Route element={<ManageUser />} path="manageuser" />
+            <Route element={<AdminProfile />} path="adminprofile" />
+          </Route>
 
-        <Route element={<User />} path="user">
-          <Route element={<Userprofile />} path="userprofile" />
-        </Route>
+          <Route element={<User />} path="user">
+            <Route element={<Userprofile />} path="userprofile" />
+          </Route>
 
-        <Route element={<Blog />} path="blog">
-
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+          <Route element={<Blog />} path="blog"></Route>
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,162 +1,119 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import './Header.css';
+import { UserContext } from "./UseContext";
 
 const Header = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const navigate = useNavigate();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+  const { loggedIn, setLoggedIn, currentUser } = useContext(UserContext);
+  const logout = () => {
+    sessionStorage.removeItem("user")
+    setLoggedIn(false)
+    navigate('/login');
+  }
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+    <div>
+      {/* <!-- Navbar --> */}
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  {/* <!-- Container wrapper --> */}
+  <div class="container-fluid">
+    {/* <!-- Toggle button --> */}
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+    {/* <!-- Collapsible wrapper --> */}
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      {/* <!-- Navbar brand --> */}
+      <a class="navbar-brand mt-2 mt-lg-0" href="#">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+          height="15"
+          alt="MDB Logo"
+          loading="lazy"
+        />
+      </a>
+      {/* <!-- Left links --> */}
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+      </ul>
+      {/* <!-- Left links --> */}
+    </div>
+    {/* <!-- Collapsible wrapper --> */}
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    {/* <!-- Right elements --> */}
+    <div class="d-flex align-items-center">
+      {/* <!-- Icon --> */}
+      <div class="d-flex align-items-center">
+        <button type="button" className="btn btn-link px-3 me-2">
+          {/* <NavLink>Login</NavLink> */}
+          
+        </button>
+        <button type="button" className="btn btn-primary me-3">
+          Sign up for free
+        </button>
+      </div>
+
+    
+      {/* <!-- Avatar --> */}
+      <div class="dropdown">
+        <a
+          class="dropdown-toggle d-flex align-items-center hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuAvatar"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            class="rounded-circle"
+            height="25"
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+          />
+        </a>
+        <ul
+          class="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuAvatar"
+        >
+          <li>
+            <a class="dropdown-item" href="#">My profile</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Settings</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Logout</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+    {/* <!-- Right elements --> */}
+  </div>
+  {/* <!-- Container wrapper --> */}
+</nav>
+{/* <!-- Navbar --> */}
+    </div>
   );
 };
+
 export default Header;
